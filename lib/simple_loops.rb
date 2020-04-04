b0f42147f4f3310491bbc2507645de37b8ae8af5
Luis Fernandez <luis.fernandez2093@gmail.com>
2020-04-04 16:35:35 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -2,11 +2,19 @@
 
 def loop_message_five_times(string)
   counter = 0 
-  while counter < 6 do 
+  while counter < 5 do 
     puts string 
     counter += 1 
   end 
 end 
 
-
+def loop_message_n_times(string, int)
+  counter = 0   
+  while counter < int do 
+    puts string 
+    counter += 1 
+  end 
+end 
+  
+def output_array(array)
   
