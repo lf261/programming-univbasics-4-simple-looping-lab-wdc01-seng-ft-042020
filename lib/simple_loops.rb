f57f42f9249a613eb7918c2f8f012edc553bebdd
Luis Fernandez <luis.fernandez2093@gmail.com>
2020-04-04 16:52:27 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -20,7 +20,7 @@ def output_array(array)
   counter = 0 
   while counter < array.length do
     puts array[counter]
-    count += 1 
+    counter += 1 
   end 
 end 
 
@@ -28,7 +28,7 @@ def return_string_array(array)
   counter = 0 
   new_arr = []
   while counter < array.length do 
-    new_arr = array[count].to_s 
+    new_arr.push(array[count].to_s)
     counter += 1 
   end 
   new_arr
